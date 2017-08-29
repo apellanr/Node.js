45e37ed48301d8d8f976d4b91f61b1a19e31ab12
apellanr <ryan.apellanes@gmail.com>
2017-08-29 11:51:57 -0700

    command keywords

--- note-app/app.js
+++ note-app/app.js
@@ -5,6 +5,19 @@ const _ = require('lodash');
 
 const notes = require('./notes.js');
 
+var command = process.argv[2];
+console.log('Command: ', command);
+console.log(process.argv);
 
-
+if (command === 'add') {
+    console.log('adding new note');
+} else if (command === 'list') {
+    console.log('listing all notes');
+} else if (command === 'read') {
+    console.log('retrieving individual note');
+} else if (command === 'remove') {
+    console.log('removing note');
+} else {
+    console.log('command not found');
+}
 
